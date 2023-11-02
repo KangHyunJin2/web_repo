380de008280c67c9e654958b2a149f9639e29cf6
ME <ME@DESKTOP-MNQ8P1M>
2023-11-03 00:03:41 +0900

    .

--- helloWeb/src/main/webapp/js/dom3.js
+++ helloWeb/src/main/webapp/js/dom3.js
@@ -6,6 +6,8 @@ import table from './domTable.js';
 let url = 'https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=284&serviceKey=giBP07s5R8iY1FIU00JpkDiceZkxbXDFcBVWk1Qws2eTMFkxRcXP11nGYxQZVZI%2FgHch1qbxswUqDvD7J9OAHQ%3D%3D';
 let titles = ['센터id','센터명','의료원','연락처'];
 
+
+
 fetch(url)
 	// function(resolve) {return resolve.Json()} 풀어서 쓰면 이렇다
 	.then(resolve => resolve.json()) 
